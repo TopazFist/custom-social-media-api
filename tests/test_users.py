f9af110c70eff9667f21d7732565886b98435767
TopazFist <jem12@rice.edu>
2025-04-02 15:23:56 -0500

    adding test infra

---
+++ tests/test_users.py
@@ -0,0 +1,49 @@
+from fastapi.testclient import TestClient
+from app import schemas
+from app.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.config import settings
+from app.database import get_db, Base
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+
+
+client = TestClient(app)
+
+
+def test_root():
+    res = client.get("/")
+    assert res.json().get("message") == "Hello World"
+    assert res.status_code == 200
+
+
+def test_create_user():
+    res = client.post(
+        "/users/", json={"email": "hello@gmail.com", "password": "abc123"}
+    )
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello@gmail.com"
+    assert res.status_code == 201
