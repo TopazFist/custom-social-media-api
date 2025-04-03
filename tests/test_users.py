8aa06d1ac74028507a9fae9450fda6473e367e9c
TopazFist <jem12@rice.edu>
2025-04-02 22:00:29 -0500

    formatting and adding actions

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,8 +4,6 @@ from jose import jwt
 import pytest
 
 
-
-
 def test_root(client):
     res = client.get("/")
     assert res.json().get("message") == "Hello World"
@@ -33,23 +31,23 @@ def test_login_user(client, test_user):
         login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
     )
     id: str = payload.get("user_id")
-    assert id == test_user['id']
+    assert id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
 
-@pytest.mark.parametrize("email, password, status_code",[
-    ('wrongemail@gmail.com', 'password123', 403),
-    ('sanjeev@gmail.com', 'wrongpassword', 403),
-    ('wrongemail@gmail.com', 'wrongpassword', 403),
-    (None, 'password123', 403),
-    ('jem12@gmail.com', None, 403)
-])
 
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password123", 403),
+        ("sanjeev@gmail.com", "wrongpassword", 403),
+        ("wrongemail@gmail.com", "wrongpassword", 403),
+        (None, "password123", 403),
+        ("jem12@gmail.com", None, 403),
+    ],
+)
 def test_incorrecrt_login(client, email, password, status_code):
-    res = client.post(
-        "/login", data={"username": email, "password": password})
+    res = client.post("/login", data={"username": email, "password": password})
 
     assert res.status_code == status_code
-    assert res.json().get('detail') == 'Invalid credentials'
-
-    
+    assert res.json().get("detail") == "Invalid credentials"
